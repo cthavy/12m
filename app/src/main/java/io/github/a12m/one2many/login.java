ec07fe8f792ccaef8bfcba3298e4e5d439152be2
Nikko Lee <nikko0327@Nikkos-MacBook-Pro.local>
2016-06-05 15:13:47 -0700

    login

---
+++ app/src/main/java/io/github/a12m/one2many/login.java
@@ -0,0 +1,23 @@
+package io.github.a12m.one2many;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(getBaseContext(), obby.class);
+        startActivity(intent);
+
+
+    }
+}
