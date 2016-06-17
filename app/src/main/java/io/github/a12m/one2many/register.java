b6eb9d1b6113c209af819f8df6a99f1cdee30ed4
Casey <casey.thavy@sjsu.edu>
2016-06-17 15:36:37 -0700

    added register page

--- app/src/main/java/io/github/a12m/one2many/register.java
+++ app/src/main/java/io/github/a12m/one2many/register.java
@@ -1,7 +1,9 @@
 package io.github.a12m.one2many;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class register extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void register_new_user(View view){
+        Intent intent = new Intent(getBaseContext(), lobby.class);
+        startActivity(intent);
     }
 }
